07be9b5c81b97a2d7a5a754442fe1b635ba4ff6e
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-04-18 21:37:52 +0200

    added Interface for accounts

---
+++ d-finanztracker-domain/src/main/java/de/dhbw/finanztracker/Domain/account/IAccount.java
@@ -0,0 +1,12 @@
+package de.dhbw.finanztracker.Domain.account;
+
+import java.util.List;
+import de.dhbw.finanztracker.Domain.account.transaction.ITransaction;
+
+interface IAccount {
+    double getBalance();
+    String getAccountNumber();
+    List<ITransaction> getTransactionHistory();
+    Boolean addTransaction(ITransaction transaction);
+    Boolean removeTransaction(ITransaction transaction);
+}
