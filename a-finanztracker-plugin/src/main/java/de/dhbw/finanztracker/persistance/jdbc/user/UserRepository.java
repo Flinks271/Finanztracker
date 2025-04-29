821ef06eedadb81cf0619bac25076791fe51481b
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-04-29 18:06:58 +0200

    added the same base for user

--- a-finanztracker-plugin/src/main/java/de/dhbw/finanztracker/persistance/jdbc/user/UserRepository.java
+++ a-finanztracker-plugin/src/main/java/de/dhbw/finanztracker/persistance/jdbc/user/UserRepository.java
@@ -1,5 +1,32 @@
 package de.dhbw.finanztracker.persistance.jdbc.user;
 
-public class UserRepository implem{
+import de.dhbw.finanztracker.domain.user.IUserRepository;
+
+import java.sql.ResultSet;
+import java.util.UUID;
+
+public class UserRepository implements IUserRepository {
+
+    @Override
+    public ResultSet getAllUsers() {
+        // Implement the logic to retrieve all users from the database
+        return null; // Placeholder return statement
+    }
+
+    @Override
+    public ResultSet getUserById(UUID accountId) {
+        // Implement the logic to retrieve a user by ID from the database
+        return null; // Placeholder return statement
+    }
+
+    @Override
+    public void saveUser(String query) {
+        // Implement the logic to save a user to the database
+    }
+
+    @Override
+    public void deleteUser(UUID accountId) {
+        // Implement the logic to delete a user from the database
+    }
     
 }
---
+++ b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/controller/TransformData/TransformUserdata.java
@@ -0,0 +1,13 @@
+package de.dhbw.finanztracker.controller.TransformData;
+
+import de.dhbw.finanztracker.domain.user.User;
+
+public class TransformUserdata {
+
+
+    public User TransformUser(){
+        return null;
+
+    }
+    
+}
