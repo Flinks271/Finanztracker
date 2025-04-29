5cc5336c5003fe68c823e605fb1b630c09382abf
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-04-29 17:10:45 +0200

    added Flyway migration to the start of the application

--- a-finanztracker-plugin/src/main/java/de/dhbw/finanztracker/App.java
+++ a-finanztracker-plugin/src/main/java/de/dhbw/finanztracker/App.java
@@ -1,5 +1,9 @@
 package de.dhbw.finanztracker;
 
+
+import org.flywaydb.core.Flyway;
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class App {
 
     public void run() {
@@ -7,7 +11,28 @@ public class App {
         // Initialize services, load UI, etc.
         clearScreen();
         System.out.println("Welcome to the FinanzTracker Application!");
+
+        migrateDatabase();
         
+    }
+
+    public static void migrateDatabase() {
+        Dotenv dotenv = Dotenv.load();
+        String dbUrl = dotenv.get("POSTGRES_URL");
+        String dbUser = dotenv.get("POSTGRES_USER");
+        String dbPassword = dotenv.get("POSTGRES_PASSWORD");
+
+        System.out.println("Migrating database...");
+
+        System.err.println("DB_URL: " + dbUrl);
+        
+        Flyway flyway = Flyway.configure()
+                .dataSource(dbUrl, dbUser, dbPassword)
+                .load();
+        flyway.migrate();
+
+        System.out.println("Flyway migrations completed.");
+        System.out.println("Application is running...");
     }
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
--- a-finanztracker-plugin/src/main/java/de/dhbw/finanztracker/Main.java
+++ a-finanztracker-plugin/src/main/java/de/dhbw/finanztracker/Main.java
@@ -2,7 +2,7 @@ package de.dhbw.finanztracker;
 
 public class Main {
     public static void main(String[] args) {
-        App app = new App(); // Create an instance of the application
-        app.run();           // Start the application
+        App app = new App();
+        app.run();
     }
 }
