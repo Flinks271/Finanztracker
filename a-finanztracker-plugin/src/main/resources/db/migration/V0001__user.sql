c17386e414b190d8f3ea593ade01ffadc13da3f6
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-04-29 17:24:54 +0200

    added user plus counter in accounts

---
+++ a-finanztracker-plugin/src/main/resources/db/migration/V0001__user.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user (
+    user_id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    username VARCHAR(255) NOT NULL,
+);
--- a-finanztracker-plugin/src/main/resources/db/migration/V0002__accounts.sql
+++ a-finanztracker-plugin/src/main/resources/db/migration/V0002__accounts.sql
@@ -1,7 +1,8 @@
 CREATE TABLE accounts (
     account_id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
-    balance NUMERIC NOT NULL,
+    balance NUMERIC(10, 2) NOT NULL,
     account_name VARCHAR(255) NOT NULL,
-    bank_name VARCHAR(255) NOT NULL
+    bank_name VARCHAR(255) NOT NULL,
+    counter SMALLINT NOT NULL,
 );
