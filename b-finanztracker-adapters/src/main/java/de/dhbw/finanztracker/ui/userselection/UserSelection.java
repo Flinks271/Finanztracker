5f02eba0c0520719bea7a0d33f8ac10954452dbc
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-04-30 23:03:53 +0200

    updated newUserRegistration

--- b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/userselection/UserSelection.java
+++ b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/userselection/UserSelection.java
@@ -3,17 +3,25 @@ package de.dhbw.finanztracker.ui.userselection;
 import de.dhbw.finanztracker.domain.IRepository;
 import de.dhbw.finanztracker.user_use_cases.user_registration.UserRegistration;
 import de.dhbw.finanztracker.domain.user.User;
+import de.dhbw.finanztracker.ui.TerminalUtility;
 
 public class UserSelection {
     
-    public static void newUserRegistration(IRepository repository) {
+    public static User newUserRegistration(IRepository repository) {
+        User user = null;
+        TerminalUtility.clearScreen();
         System.out.println("Please enter a username: ");
         try (java.util.Scanner scanner = new java.util.Scanner(System.in)) {
+
             String inputUsername = scanner.nextLine();
-            User user = UserRegistration.registerUser(inputUsername, repository);
+            user = UserRegistration.registerUser(inputUsername, repository);
             System.out.println("User " + inputUsername + " has been registered successfully.");
+            
+            Thread.sleep(1000); 
+            
         } catch (Exception e) {
             System.err.println("An error occurred during user registration: " + e.getMessage());
         }
+        return user;
     }
 }
