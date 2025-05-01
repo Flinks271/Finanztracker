0abf691cb4144b1cdb72aa162f7587d28cb971ba
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-05-01 14:55:38 +0200

    start for singular account overview

---
+++ b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/controller/AccountOverviewController.java
@@ -0,0 +1,15 @@
+package de.dhbw.finanztracker.controller;
+
+import de.dhbw.finanztracker.domain.IRepository;
+import de.dhbw.finanztracker.domain.account.IAccount;
+import de.dhbw.finanztracker.ui.TerminalUtility;
+
+import java.util.List;
+
+public class AccountOverviewController {
+
+    public static void Start(List<IRepository> repositories,IAccount account, TerminalUtility terminalUtility) {
+        
+    }
+    
+}
--- b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/accountInteractions/SingularAccountOverview.java
+++ b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/accountInteractions/SingularAccountOverview.java
@@ -1,5 +1,16 @@
 package de.dhbw.finanztracker.ui.accountInteractions;
 
+import java.util.List;
+
+import de.dhbw.finanztracker.domain.IRepository;
+import de.dhbw.finanztracker.domain.user.User;
+import de.dhbw.finanztracker.ui.TerminalUtility;
+
 public class SingularAccountOverview {
+
+    public static void showAccountdetails(User user, List<IRepository> repositories, TerminalUtility terminalUtility) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showAccountdetails'");
+    }
     
 }
