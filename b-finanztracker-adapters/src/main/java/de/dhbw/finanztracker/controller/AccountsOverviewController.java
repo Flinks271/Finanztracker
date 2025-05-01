380e5390bec5612b7caf802d59a4cf605470e258
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-05-01 12:38:32 +0200

    added Account Overview

--- b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/controller/AccountsOverviewController.java
+++ b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/controller/AccountsOverviewController.java
@@ -1,8 +1,11 @@
 package de.dhbw.finanztracker.controller;
 
+import de.dhbw.finanztracker.TransformData.TransformAccountdata;
 import de.dhbw.finanztracker.domain.IRepository;
+import de.dhbw.finanztracker.domain.account.IAccount;
 import de.dhbw.finanztracker.domain.user.User;
 import de.dhbw.finanztracker.ui.TerminalUtility;
+import de.dhbw.finanztracker.ui.accountInteractions.AccountsOverview;
 import de.dhbw.finanztracker.ui.accountInteractions.CreateNewAccount;
 
 import java.util.List;
@@ -15,17 +18,19 @@ public class AccountsOverviewController {
         System.out.println("Loading accounts for user: " + user.getUsername());
         IRepository accountRepository = repositories.get(0);
         List<Map<String, Object>> result = accountRepository.getWhere("user_id = '" + user.getUserId() + "'");
+        System.out.println("Result: " + result);
 
         if (result.isEmpty()) {
             CreateNewAccount.createAccount(accountRepository, user, terminalUtility);
         } else {
-            
-            
+            List<IAccount> accounts = TransformAccountdata.TransformAccounts(result);
+            user.addAccounts(accounts);
         }
+
+        System.out.println("Loading accounts for user: " + user.getUsername());
+
+        AccountsOverview.showAccounts(user, repositories, terminalUtility);        
         
-
-
-
     }
     
 }
--- b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/accountInteractions/AccountsOverview.java
+++ b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/accountInteractions/AccountsOverview.java
@@ -1,5 +1,61 @@
 package de.dhbw.finanztracker.ui.accountInteractions;
 
+import de.dhbw.finanztracker.domain.IRepository;
+import de.dhbw.finanztracker.domain.account.IAccount;
+import de.dhbw.finanztracker.domain.user.User;
+import de.dhbw.finanztracker.ui.TerminalUtility;
+
+import java.util.List;
+
 public class AccountsOverview {
+
+    public static void showAccounts(User user, List<IRepository> repositories, TerminalUtility terminalUtility) {
+        terminalUtility.clearScreen();
+
+        List<IAccount> accounts = user.getAccounts();
+
+        if (accounts.isEmpty()) {
+            System.out.println("No accounts found for user: " + user.getUsername());
+            System.out.println("Please create a new account.");
+            CreateNewAccount.createAccount(repositories.get(0), user,terminalUtility);
+            return;
+        }
+        
+        showAccountsTable(accounts);
+    }
+
+
+
+    public static void showAccountsTable(List<IAccount> accounts) {
+  
+        int maxIndexWidth = String.valueOf(accounts.size()).length();
+        maxIndexWidth = Math.max(maxIndexWidth, 2); 
+        int maxNameWidth = accounts.stream()
+                .map(a -> a.getAccountName().length())
+                .max(Integer::compare)
+                .orElse(0);
+        maxNameWidth = Math.max(maxNameWidth, 12);
+        int maxBankNameWidth = accounts.stream()
+                .map(a -> a.getBankName().length())
+                .max(Integer::compare)
+                .orElse(0);
+        maxBankNameWidth = Math.max(maxBankNameWidth, 9);
+        int maxBalanceWidth = accounts.stream()
+                .map(a -> String.valueOf(a.getBalance()).length() + 2)
+                .max(Integer::compare)
+                .orElse(0);
+        maxBalanceWidth = Math.max(maxBalanceWidth, 7);
+    
+        System.out.printf("%-" + maxIndexWidth + "s | %-" + maxNameWidth + "s | %-" + maxBankNameWidth + "s | %-" + maxBalanceWidth + "s%n",
+                "Nr", "Account Name","Bank Name", "Balance");
+        System.out.println("-".repeat(maxIndexWidth) + "-+-" + "-".repeat(maxNameWidth) + "-+-" + "-".repeat(maxBankNameWidth) + "-+-" + "-".repeat(maxBalanceWidth));
+    
+        for (int i = 0; i < accounts.size(); i++) {
+            IAccount acc = accounts.get(i);
+            System.out.printf("%-" + maxIndexWidth + "s | %-" + maxNameWidth + "s | %-" + maxBankNameWidth + "s | %-" + maxBalanceWidth + "s%n",
+                    i + 1, acc.getAccountName(), acc.getBankName(), acc.getBalance() + " €");
+        }
+        
+    }
     
 }
