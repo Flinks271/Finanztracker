4e9f44346690924c396ec1ced242b267e759cc84
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-05-01 12:59:42 +0200

    added total Balance to the table

--- b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/accountInteractions/AccountsOverview.java
+++ b-finanztracker-adapters/src/main/java/de/dhbw/finanztracker/ui/accountInteractions/AccountsOverview.java
@@ -6,6 +6,7 @@ import de.dhbw.finanztracker.domain.user.User;
 import de.dhbw.finanztracker.ui.TerminalUtility;
 
 import java.util.List;
+import java.util.concurrent.atomic.DoubleAdder;
 
 public class AccountsOverview {
 
@@ -40,22 +41,27 @@ public class AccountsOverview {
                 .max(Integer::compare)
                 .orElse(0);
         maxBankNameWidth = Math.max(maxBankNameWidth, 9);
+        DoubleAdder totalBalance = new DoubleAdder();
         int maxBalanceWidth = accounts.stream()
-                .map(a -> String.valueOf(a.getBalance()).length() + 2)
+                .peek(a -> totalBalance.add(a.getBalance()))
+                .map(a -> String.valueOf(a.getBalance()).length())
                 .max(Integer::compare)
                 .orElse(0);
         maxBalanceWidth = Math.max(maxBalanceWidth, 7);
     
-        System.out.printf("%-" + maxIndexWidth + "s | %-" + maxNameWidth + "s | %-" + maxBankNameWidth + "s | %-" + maxBalanceWidth + "s%n",
+        System.out.printf("%-" + maxIndexWidth + "s | %-" + maxNameWidth + "s | %-" + maxBankNameWidth + "s | %-" + maxBalanceWidth + "s %n",
                 "Nr", "Account Name","Bank Name", "Balance");
-        System.out.println("-".repeat(maxIndexWidth) + "-+-" + "-".repeat(maxNameWidth) + "-+-" + "-".repeat(maxBankNameWidth) + "-+-" + "-".repeat(maxBalanceWidth));
+        System.out.println("-".repeat(maxIndexWidth) + "-+-" + "-".repeat(maxNameWidth) + "-+-" + "-".repeat(maxBankNameWidth) + "-+---" + "-".repeat(maxBalanceWidth));
     
         for (int i = 0; i < accounts.size(); i++) {
             IAccount acc = accounts.get(i);
-            System.out.printf("%-" + maxIndexWidth + "s | %-" + maxNameWidth + "s | %-" + maxBankNameWidth + "s | %-" + maxBalanceWidth + "s%n",
-                    i + 1, acc.getAccountName(), acc.getBankName(), acc.getBalance() + " €");
+            System.out.printf("%-" + maxIndexWidth + "s | %-" + maxNameWidth + "s | %-" + maxBankNameWidth + "s | %-" + maxBalanceWidth + ".2f € %n",
+                    i + 1, acc.getAccountName(), acc.getBankName(), acc.getBalance());
         }
-        
+        System.out.println("-".repeat(maxIndexWidth) + "-┴-" + "-".repeat(maxNameWidth) + "-┴-" + "-".repeat(maxBankNameWidth) + "-+---" + "-".repeat(maxBalanceWidth));
+    
+        System.out.printf("%-" + (maxIndexWidth + maxNameWidth + maxBankNameWidth + 6) + "s | %-" + maxBalanceWidth + ".2f € %n",
+            "Total Balance",  totalBalance.doubleValue());
     }
     
 }
