8a21829458b0c551b128b6923f293fcd34f08a88
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-05-02 22:03:58 +0200

    kommentieren für codesmell nein weil ich wollte

--- d-finanztracker-domain/src/main/java/de/dhbw/finanztracker/Domain/IRepository.java
+++ d-finanztracker-domain/src/main/java/de/dhbw/finanztracker/Domain/IRepository.java
@@ -3,6 +3,12 @@ package de.dhbw.finanztracker.domain;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This interface defines the basic CRUD operations for a repository.
+ * It is used to interact with the data layer of the application.
+ * 
+ * Hätte man besser machen können, wenn man es etwa dynamischer gemacht hätte und nicht für jede tabelle ein eigenes repositorymachen müsste.
+ */
 public interface IRepository {
     List<Map<String, Object>> getAll();
     List<Map<String, Object>> getCond(String condition);
