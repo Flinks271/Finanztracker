0c34996318ad51985eeadfedd679098e674bc13a
Flinks271 <63011166+Flinks271@users.noreply.github.com>
2025-05-02 20:07:15 +0200

    corrected order

--- a-finanztracker-plugin/src/main/resources/db/migration/V0003__tansactions.sql
+++ a-finanztracker-plugin/src/main/resources/db/migration/V0003__tansactions.sql
@@ -1,31 +1,29 @@
+CREATE Table counterparty (
+    counterparty_id UUID PRIMARY KEY,
+    counterparty_name VARCHAR(64) NOT NULL,
+    counterparty_description TEXT
+);
 CREATE TABLE transactions (
-    transaction_id SERIAL,
+    transaction_id SERIAL PRIMARY KEY,
     bank_account_id UUID NOT NULL REFERENCES accounts(account_id)ON DELETE CASCADE ON UPDATE CASCADE, 
     amount DECIMAL(15, 2) NOT NULL,
     transaction_description TEXT, 
     execution_date DATE NOT NULL, 
-    entry_date DATE NOT NUL DEFAULT CURRENT_DATE, 
+    entry_date DATE NOT NULL DEFAULT CURRENT_DATE, 
     last_modified_date DATE NOT NULL DEFAULT CURRENT_DATE,
-    counterparty counterparty_id NOT NULL REFERENCES counterparty(counterparty_id)ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (transaction_id, bank_account_id)
+    counterparty_id UUID NOT NULL REFERENCES counterparty(counterparty_id)ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE Table categories (
     category_name VARCHAR(64) PRIMARY KEY,
-    category_description TEXT,
+    category_description TEXT
 );
 CREATE TABLE relation_transaction_category (
     transaction_id SERIAL NOT NULL REFERENCES transactions(transaction_id)ON DELETE CASCADE ON UPDATE CASCADE,
-    bank_account_id UUID NOT NULL REFERENCES accounts(account_id)ON DELETE CASCADE ON UPDATE CASCADE, 
     category_name VARCHAR(64) NOT NULL REFERENCES categories(category_name)ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (transaction_id, bank_account_id, category_name)
-);
-CREATE Table counterparty (
-    counterparty_id UUID PRIMARY KEY,
-    counterparty_name VARCHAR(64) NOT NULL,
-    counterparty_description TEXT,
+    PRIMARY KEY (transaction_id, category_name)
 );
 CREATE TABLE reaccuring (
-    reaccuring_id SERIAL,
+    reaccuring_id SERIAL PRIMARY KEY,
     bank_account_id UUID NOT NULL REFERENCES accounts(account_id)ON DELETE CASCADE ON UPDATE CASCADE,
     reaccuring_name VARCHAR(64) NOT NULL,
     reaccuring_description TEXT,
@@ -33,12 +31,10 @@ CREATE TABLE reaccuring (
     reaccuring_start_date DATE NOT NULL,
     reaccuring_last_modified_date DATE NOT NULL DEFAULT CURRENT_DATE,
     reaccuring_end_date DATE,
-    interval_in_days INT NOT NULL DEFAULT 28,
-    PRIMARY KEY (reaccuring_id, bank_account_id)
+    interval_in_days INT NOT NULL DEFAULT 28
 );
 CREATE TABLE relation_reaccuring_category (
     reaccuring_id SERIAL NOT NULL REFERENCES reaccuring(reaccuring_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    bank_account_id UUID NOT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
     category_name VARCHAR(64) NOT NULL REFERENCES categories(category_name) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (reaccuring_id, bank_account_id, category_name)
+    PRIMARY KEY (reaccuring_id, category_name)
 );
